43777b7ad093ad46e2f80545fe422f543289a7d2
budujoseph <budu.joseph95@gmail.com>
2025-07-13 14:04:41 +0000

    changes

--- exam.py
+++ exam.py
@@ -1,4 +1,4 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
-print(f"Your name is {first_name, {last_name}}")
+print(f"Your name is {first_name}, {last_name}")
